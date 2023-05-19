2001a2517229614b4ac2bae94a71180377a67c32
Tran Duc Linh <duclinhlmht92@gmail.com>
2023-05-19 13:04:24 +0700

    Homepage

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -1,31 +1,106 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './HomeHeader.scss';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./HomeHeader.scss";
+import logo from '../../assets/logo.svg';
 class HomeHeader extends Component {
-
-    render() {
-       
-
-        return (
-            <div className='home-header-container'>
-                <div className='home-header-content'>
-
-                </div>
+  render() {
+    return (
+      <React.Fragment>
+        <div className="home-header-container">
+          <div className="home-header-content">
+            <div className="left-content">
+              <i className="fas fa-bars"></i>
+              <img className="header-logo" src={logo}></img>
             </div>
-        );
-    }
-
+            <div className="center-content">
+              <div className="child-content">
+                <div>
+                  <b>Chuyên khoa</b>
+                </div>
+                <div className="subs-title">Tìm bác sĩ theo chuyên khoa</div>
+              </div>
+              <div className="child-content">
+                <div>
+                  <b>Cơ sở y tế</b>
+                </div>
+                <div className="subs-title">Chọn bệnh viện phòng khám</div>
+              </div>
+              <div className="child-content">
+                <div>
+                  <b>Bác sĩ</b>
+                </div>
+                <div className="subs-title">Chọn bác sĩ giỏi</div>
+              </div>
+              <div className="child-content">
+                <div>
+                  <b>Gói khám</b>
+                </div>
+                <div className="subs-title">Khám sức khỏe tổng quát</div>
+              </div>
+            </div>
+            <div className="right-content">
+              <div className="support">
+                <i className="fas fa-question-circle"></i>Hỗ trợ
+              </div>
+              <div className="language-vi active">VN</div>
+              <div className="language-en active">EN</div>
+            </div>
+          </div>
+        </div>
+        <div className="home-header-banner">
+          <div className="content-up">
+            <div className="title1">NỀN TẢNG Y TẾ</div>
+            <div className="title2">CHĂM SÓC SỨC KHỎE TOÀN DIỆN</div>
+            <div className="search">
+              <i className="fas fa-search"></i>
+              <input type="text" placeholder="Tìm chuyên khoa khám bệnh" />
+            </div>
+          </div>
+          <div className="content-down">
+            <div className="options">
+                <div className="option-child">
+                    <div className="icon-child"><i className="far fa-hospital"></i></div>
+                    <div className="text-child">Khám chuyên khoa</div>
+                </div>
+                <div className="option-child">
+                    <div className="icon-child"><i className="fas fa-mobile-alt"></i></div>
+                    <div className="text-child">Khám từ xa</div>
+                </div>
+                <div className="option-child">
+                    <div className="icon-child"><i className="fas fa-procedures"></i></div>
+                    <div className="text-child">Khám tổng quát</div>
+                </div>
+                <div className="option-child">
+                    <div className="icon-child"><i className="fas fa-flask"></i></div>
+                    <div className="text-child">Xét nghiệm y học</div>
+                </div>
+                <div className="option-child">
+                    <div className="icon-child"><i className="fas fa-user-md"></i></div>
+                    <div className="text-child">Sức khỏe tinh thần</div>
+                </div>
+                <div className="option-child">
+                    <div className="icon-child"><i className="fas fa-briefcase-medical"></i></div>
+                    <div className="text-child">Khám nha khoa</div>
+                </div>
+                
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-        isLoggedIn: state.user.isLoggedIn
-    };
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+    language: state.app.language,
+    //inject
+  };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeHeader);
